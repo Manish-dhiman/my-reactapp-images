ba6deec50d052eaddc191b5c5ab0421e28408be1
ankushtechtweek <92977536+ankushtechtweek@users.noreply.github.com>
2023-05-05 18:37:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - it's awesome!!
+          Learn React - it's not awesome!!
         </a>
       </header>
     </div>
